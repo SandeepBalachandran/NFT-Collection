b81e317839d33d5e01bb363b197dc2d7ac229610
sandeepbalachandran <sandeepbalachandran2@gmail.com>
2022-05-05 20:54:40 +0530

    Initial Commit

---
+++ hardhat-tutoiral/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xd6987f742513f59a9F323DAFd6749155841752A5";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://whitelist-dapp-a1ibfz3xn-sandeepbalachandran.vercel.app/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
